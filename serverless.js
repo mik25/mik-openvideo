9c46af892251904f1f2e53a721e475b499051bc6
mik25 <mikmc55@gmail.com>
2023-03-01 09:37:36 +0000

    Update serverless.js

--- serverless.js
+++ serverless.js
@@ -4,8 +4,9 @@ const addonInterface = require('./index')
 const router = getRouter(addonInterface)
 
 module.exports = (req, res) => {
-	router(req, res, () => {
-		res.statusCode = 404
-		res.end()
-	})
+  router(req, res, () => {
+    res.statusCode = 404
+    res.end()
+  })
 }
+
